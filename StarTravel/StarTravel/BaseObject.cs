35123a33bc1147f55637889ab947c1466d96afbf
AndreyKotelnikov <Dragon1983@yandex.ru>
2019-05-02 15:28:01 +0300

    Немного доработал пример из методички.

---
+++ StarTravel/StarTravel/BaseObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarTravel
+{
+    class BaseObject
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+        protected string number;
+        public BaseObject(Point pos, Point dir, Size size, string number = "")
+        {
+            Pos = pos;
+            Dir = dir;
+            Size = size;
+            this.number = number;
+        }
+        public virtual void Draw()
+        {
+            Game.Buffer.Graphics.DrawEllipse(Pens.White, Pos.X, Pos.Y, Size.Width, Size.Height);
+            Font font = new Font("Verdana", 10);
+            SolidBrush myBrush = new SolidBrush(Color.White);
+            Game.Buffer.Graphics.DrawString(number, font, myBrush, Pos.X + 1, Pos.Y + 1);
+        }
+        public virtual void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            Pos.Y = Pos.Y + Dir.Y;
+            if (Pos.X < 0) Dir.X = -Dir.X;
+            if (Pos.X + Size.Width > Game.Width) Dir.X = -Dir.X;
+            if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            if (Pos.Y + Size.Height > Game.Height) Dir.Y = -Dir.Y;
+        }
+    }
+
+}
